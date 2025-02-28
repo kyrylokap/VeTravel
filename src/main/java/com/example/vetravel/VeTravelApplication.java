be8be2965592b42f23ecb2a52c1a6d747a5b235c
kyrylokap <163316652+kyrylokap@users.noreply.github.com>
2025-02-28 22:05:50 +0100

    Started developing 28.02.2025

---
+++ src/main/java/com/example/vetravel/VeTravelApplication.java
@@ -0,0 +1,13 @@
+package com.example.vetravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeTravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeTravelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vetravel/VeTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vetravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeTravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
